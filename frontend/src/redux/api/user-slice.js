3afbc1202f76b8a8253a355c27c43ba20d9a5bb5
Chigala Enyeazu <chigala11@gmail.com>
2022-06-23 23:07:42 +0100

    add my heroku appurl

--- frontend/src/redux/api/user-slice.js
+++ frontend/src/redux/api/user-slice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const userApi = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://lister-app.herokuapp.com/api' }),
   tagTypes: ['Product', 'List', 'category'],
   endpoints: builder => ({
     getProfile: builder.mutation({
